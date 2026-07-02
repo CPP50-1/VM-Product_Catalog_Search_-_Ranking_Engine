02d732fd89d2617a131106ec78663cbcf74ffdcd
GSoMan-dev <ayotte.victor@gmail.com>
2026-07-02 12:19:05 +0200

    feat : implementing index to search
    score must be redone as we only haves IDs in the index

--- engine/ranking.py
+++ engine/ranking.py
@@ -2,6 +2,7 @@ import heapq
 import math
 
 from engine.tokenize import tokenize
+from engine.index import Index
 
 def get_score(max_tokens: int, product: dict) -> float:
     return ((product['match']/max_tokens)*0.5 +
@@ -13,17 +14,19 @@ def search(query: str, top_k:int = 10):
     tokens = tokenize(query)
     results = {}
     searched_list= []
+    index = Index()
 
     for token in tokens:
-        list_product = [] # todo use index to get a product list for each token
+        list_product = index[token]
         for product in list_product:
-            if results.get(product['id']):
-                results[product['id']]['match'] += 1
-            else: results[product['id']] = {'match': 1, 'product': product}
+            if results.get(product):
+                results[product]['match'] += 1
+            else: results[product] = {'match': 1}
 
     for result in results:
-        score = get_score(len(tokens), result)
-        heapq.heappush(searched_list, (score, result['product']))
+        # score = get_score(len(tokens), result)
+        heapq.heappush(searched_list, (1, result))
         if len(searched_list) > top_k:
             heapq.heappop(searched_list)
     return searched_list
+print(search('bluetooth',1000))
