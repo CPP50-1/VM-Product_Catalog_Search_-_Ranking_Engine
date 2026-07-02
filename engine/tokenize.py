2dd86d103ed4b2e9963fcc9dee0a7538e08dbcc9
GSoMan-dev <ayotte.victor@gmail.com>
2026-07-02 08:58:53 +0200

    comments : description + comments
    added function description and comments

--- engine/tokenize.py
+++ engine/tokenize.py
@@ -1,7 +1,11 @@
 import re
 
 def tokenize(text: str):
+    '''Returns a list of tokens from the given text, ignoring punctuation and 2- letters words'''
+    # removes non-letters
     text = re.sub( r'[^\w\s]', ' ', text)
+    # removes words that are 2 or less letters
     text = re.sub( r'\W*\b\w{1,2}\b', ' ', text)
+    # removes multiple spaces
     text = re.sub(' +', ' ', text).strip()
     return re.split(' ', text)
