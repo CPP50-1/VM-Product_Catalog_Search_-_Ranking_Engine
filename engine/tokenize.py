953411603628849c8310e0c55a683830614051e6
GSoMan-dev <ayotte.victor@gmail.com>
2026-07-02 09:42:21 +0200

    feat : tokenize
    Merge pull request #1 from CPP50-1/feat/tokenize

---
+++ engine/tokenize.py
@@ -0,0 +1,14 @@
+import re
+
+def tokenize(text: str):
+    """Returns a list of tokens from the given text, ignoring punctuation and 2- letters words"""
+    # removes non-letters
+    text = re.sub( r'[^\w\s]', ' ', text)
+    # removes words that are 2 or less letters
+    text = re.sub( r'\W*\b\w{1,2}\b', ' ', text)
+    # removes multiple spaces
+    text = re.sub(' +', ' ', text).strip()
+    if text:
+        return re.split(' ', text)
+    else:
+        return []
