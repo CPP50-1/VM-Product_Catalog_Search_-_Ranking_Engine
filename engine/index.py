eb5c8eca64d5152c10657ddf452c04911fafee6d
R3dn0 <64706773+R3dn0@users.noreply.github.com>
2026-07-03 11:52:11 +0200

    chore: sync with main

--- engine/index.py
+++ engine/index.py
@@ -4,29 +4,49 @@ from .tokenize import tokenize
 
 
 class Index:
-    _instance: dict[str, list[str]] | None = None
+    _index: dict[str, list[str]] | None = None
+    _catalog: dict[str, list[dict[str, dict]]] | None = None 
 
-    def __new__(cls) -> dict[str, list[str]]:
-        if cls._instance is None:
-            print("Building index... faster than a salesperson on commission.")
+    @classmethod
+    def get_index(cls):
+        """Return dict[str, list[str]]"""
+        if cls._index is None:
+            cls._load()
+        return cls._index
+
+    @classmethod
+    def get_catalog(cls):
+        """Return dict[str, list[dict]]"""
+        if cls._catalog is None:
+            cls._load()
+        return cls._catalog
+
+    @classmethod
+    def _load(cls):
+        if cls._index is None:
+            print("Building index and catalog... faster than a salesperson on commission.")
 
             start = perf_counter()
 
-            cls._instance = {}
+            cls._index = {}
+            cls._catalog = {}
+
             with open("catalog.json", "r") as f:
                 data = json.load(f)
                 for product in data:
+                    cls._catalog[product["id"]] = product
+
+
                     indexs_str = product["name"] + " " + " ".join(product["tags"])
                     indexs = tokenize(indexs_str)
                     for index in indexs:
-                        cls._instance.setdefault(str(index), []).append(product["id"])
+                        cls._index.setdefault(str(index), []).append(product["id"])
 
             elapsed = perf_counter() - start
-            print(f"Index built. {len(data)} products tamed in {elapsed:.3f}s.")
-
-        return cls._instance
+            print(f"They're built. {len(data)} products tamed in {elapsed:.3f}s.")
 
     @classmethod
     def reset(cls) -> None:
         """For test isolation only ! Never use it elsewhere !"""
-        cls._instance = None
+        cls._index = None
+        cls._catalog = None
---
+++ engine/ranking.py
@@ -0,0 +1,34 @@
+import heapq
+import math
+
+from engine.tokenize import tokenize
+from engine.index import Index
+
+def get_score(max_tokens: int, product_tokens: int, product: dict) -> float:
+    """Gives score for a product based on number of tokens in search, stock and rank"""
+    return ((product_tokens/max_tokens)*0.5 +
+            product['stock']*0.2 +
+            (1/ math.log2(product['sales_rank'] + 2))*0.3)
+
+
+def search(query: str, top_k:int = 10):
+    """returns a list of tuple (score, product), ordered by score and limited to top_k items"""
+    tokens = tokenize(query)
+    matches = {}
+    searched_list= []
+    index = Index().get_index()
+    catalog = Index().get_catalog()
+
+    for token in tokens:
+        list_product = index.get(token) or []
+        for product in list_product:
+            matches[product] = matches.get(product, 0) + 1
+
+
+    for match_id in matches:
+        product = catalog[match_id]
+        score = get_score(len(tokens), matches[match_id], product)
+        heapq.heappush(searched_list, (score, product))
+        if len(searched_list) > top_k:
+            heapq.heappop(searched_list)
+    return sorted(searched_list, reverse=True)
