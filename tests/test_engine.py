6f38338bd5914da6b6d5de4d39d7688476eccd61
GSoMan-dev <ayotte.victor@gmail.com>
2026-07-06 13:57:21 +0200

    feat : TestRankings
    added tests +  descriptions

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -29,22 +29,38 @@ class TestCategories:
 class TestRanking:
 
     def test_ranking_1(self):
-        pass
+        # simple search
+        result = search(['wireless'])
+        assert (result[0][1]["id"] == "P01050") and (result[-1][1]["id"] == "P03657") and len(result) == 10
 
     def test_ranking_2(self):
-        pass
+        # multiple words search
+        result = search(['wireless', 'hub', 'keyboard'])
+        assert (result[0][1]["id"] == "P00579") and (result[-1][1]["id"] == "P04720") and len(result) == 10
 
     def test_ranking_3(self):
-        pass
+        # limiting size
+        result = search(['rechargeable'], 5)
+        assert (result[0][1]["id"] == "P01050") and (result[-1][1]["id"] == "P04234") and len(result) == 5
 
     def test_ranking_4(self):
-        pass
+        # out of range size
+        result = search(['wireless'], -1)
+        assert result == []
 
     def test_ranking_5(self):
-        pass
+        # top_k higher than results
+        # result = search(['rechargeable'], 6000)
+        result = search(['wireless'], 6000)
+        assert len(result) == 817
 
     def test_ranking_6(self):
-        pass
+        # test of a full result
+        result = search(['wireless'], 1)
+        assert result == [(100.55371566952532, {'id': 'P01050', 'name': 'Rechargeable Gaming Digital',
+                                                'category': 'Office/Furniture/Desks',
+                                                'tags': ['wireless', 'chair', 'switch', 'professional', 'adjustable'],
+                                                'price': 71.65, 'stock': 500, 'sales_rank': 46})]
 
 
 class TestSuggest:
